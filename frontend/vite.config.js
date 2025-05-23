e8ea77b3daa373ad810341b87662323bfe9c9572
hayakzan <hayakzan@10-197-128-248.sds.oregonstate.edu>
2025-05-22 17:40:32 -0700

    updated frontend port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    // port: 4003,
-    port: 3000,
+    port: 4003,
+    // port: 3000,
     strictPort: true,
     proxy: {
       '/users':             { target: 'http://localhost:5181', changeOrigin: true },

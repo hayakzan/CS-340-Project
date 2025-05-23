0d69251c7d1cb08a752ae21d3f6c624a988dfd76
Yiğit Kolat <61164329+hayakzan@users.noreply.github.com>
2025-05-22 19:02:04 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    // port: 4003,
-    port: 3000,
+    port: 4003,
     strictPort: true,
     proxy: {
       '/users':             { target: 'http://localhost:5181', changeOrigin: true },

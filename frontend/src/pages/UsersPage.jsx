250035f0d1c678d512e93e18aca0e492aa748397
hayakzan <hayakzan@10-197-139-53.sds.oregonstate.edu>
2025-05-24 16:12:45 -0700

    update

--- frontend/src/pages/UsersPage.jsx
+++ frontend/src/pages/UsersPage.jsx
@@ -56,7 +56,7 @@ export default function UsersPage() {
       <div>
   <a href="/reset-all" class="button">Reset All Data</a>
   <br />
-  <a href="/users/delete-sample-user" class="button">Delete Sample User</a>
+  <a href="/delete-sample-user" class="button">Delete Sample User</a>
 </div>
 
       <form onSubmit={handleAdd} style={{ marginBottom: '1em' }}>

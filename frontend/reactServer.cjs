54c3ac8e2d5aba193d3168e3ae51b676a6188627
Yiğit Kolat <61164329+hayakzan@users.noreply.github.com>
2025-06-02 13:29:48 -0700

    Update reactServer.cjs

--- frontend/reactServer.cjs
+++ frontend/reactServer.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 4015; 
+const PORT = 4020; 
 
 // Serve static files from the React build directory
 app.use(express.static(path.join(__dirname, 'dist')));

512df62a40c0319de07fef1040f96698738344af
Yiğit Kolat <61164329+hayakzan@users.noreply.github.com>
2025-06-02 14:39:11 -0700

    Update PL.sql

--- PL.sql
+++ PL.sql
@@ -1,14 +1,14 @@
 /*
-    PL.SQL - Project Step 4
+    PL.SQL - Project Step 5
     Group 27: Hunter Havice and Yigit Kolat
     Last Updated: 05/22/2025
 */
 
 -- Drop procedure if it exists
-DROP PROCEDURE IF EXISTS DeleteSampleUser;
+DROP PROCEDURE IF EXISTS DeleteSamplePlayer;
 
 DELIMITER //
-CREATE PROCEDURE DeleteSampleUser()
+CREATE PROCEDURE DeleteSamplePlayer()
 BEGIN
     DELETE FROM users WHERE name = 'Alex Ode';
 END //

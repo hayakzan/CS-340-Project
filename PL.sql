1f9f6b50fae737a597d724f50fd877221d81daf7
Yiğit Kolat <61164329+hayakzan@users.noreply.github.com>
2025-06-02 19:53:48 -0700

    Update PL.sql

--- PL.sql
+++ PL.sql
@@ -1,15 +1,17 @@
 /*
     PL.SQL - Project Step 5
     Group 27: Hunter Havice and Yigit Kolat
-    Last Updated: 05/22/2025
+    Last Updated: 06/02/2025
 */
 
 -- Drop procedure if it exists
 DROP PROCEDURE IF EXISTS DeleteSamplePlayer;
-
 DELIMITER //
 CREATE PROCEDURE DeleteSamplePlayer()
 BEGIN
-    DELETE FROM users WHERE name = 'Alex Ode';
+  -- remove the one with the smallest user_id
+  DELETE FROM users
+  ORDER BY user_id ASC
+  LIMIT 1;
 END //
 DELIMITER ;
